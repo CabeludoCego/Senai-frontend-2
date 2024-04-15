4c3e48e9c8eb57777f4044d27780ecb2657408c4
ArchLemos <51244569+CabeludoCego@users.noreply.github.com>
2024-04-15 18:51:02 -0300

    Task v1 - Base layout done

---
+++ js/script.js
@@ -0,0 +1,9 @@
+
+// alert("Exemplo de comando alert");
+console.log("Mensagem enviado pelo console"); 
+
+//efeito do botão voltar ao Topo
+
+//Validação de Login
+
+//Ativar alert no botão cadastrar
